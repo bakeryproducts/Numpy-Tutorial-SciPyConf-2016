4140088fead4aeb5501012032b4fc97f7e4ad5d3
bakeryproducts <gleb.m.sokolov>
2017-02-25 22:49:48 +0000

    my solution

--- exercises/load_text/bp_solution.py
+++ exercises/load_text/bp_solution.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-arr = np.loadtxt('float_data.txt')
+arr1 = np.loadtxt('float_data.txt',dtype='int32')
 
+arr2 = np.loadtxt('float_data_with_header.txt',skiprows=1)
 
-print(arr)
+arr3 = np.loadtxt('complex_data_file.txt',skiprows=1,comments='%',
+                  usecols=(0,1,2,4),dtype='int32',delimiter=','
+                  )
+print(arr3)
