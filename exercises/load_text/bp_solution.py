420002a7bbaa828be4bc7304594f7f591d3d198c
bakeryproducts <gleb.m.sokolov>
2017-02-25 23:02:41 +0000

    my solution

--- exercises/load_text/bp_solution.py
+++ exercises/load_text/bp_solution.py
@@ -4,6 +4,8 @@ arr1 = np.loadtxt('float_data.txt',dtype='int32')
 
 arr2 = np.loadtxt('float_data_with_header.txt',skiprows=1)
 
+print(arr2)
+
 arr3 = np.loadtxt('complex_data_file.txt',skiprows=1,comments='%',
                   usecols=(0,1,2,4),dtype='int32',delimiter=','
                   )
